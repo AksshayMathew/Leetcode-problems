64e7ad89668714360a4e0c106e2b0c1657733e6f
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-02-12 10:37:06 +0530

    Time: 2 ms (60.48%) | Memory: 9.5 MB (90.81%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,29 +1,40 @@
-class Solution {
-public:
-    bool isAnagram(string s, string t) 
-    {
-    unordered_map<int,int> mpp;
-        int n = s.size();
-        int m = t.size();
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) 
+//     {
+//     unordered_map<int,int> mpp;
+//         int n = s.size();
+//         int m = t.size();
 
-        for(int i=0; i<n; i++)
-        {
-           mpp[s[i]]++;
-        }
+//         for(int i=0; i<n; i++)
+//         {
+//            mpp[s[i]]++;
+//         }
 
-          for(int i=0; i<m; i++)
-        {
-           mpp[t[i]]--;
-        }
+//           for(int i=0; i<m; i++)
+//         {
+//            mpp[t[i]]--;
+//         }
 
 
-        for(auto it: mpp)
-        {
-            if( (n !=m ) || (it.second != 0) )
-            return false;
-        }
+//         for(auto it: mpp)
+//         {
+//             if( (n !=m ) || (it.second != 0) )
+//             return false;
+//         }
 
-        return true;
+//         return true;
         
+//     }
+// };
+
+
+class Solution 
+{
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
     }
 };
