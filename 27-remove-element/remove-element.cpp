4df272e1f2cca0839ae0276b056a1b1da675b703
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-02-15 21:08:59 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (5.20%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val)
+  int removeElement(vector<int>& nums, int val)
+ {  
+   
+   int n= nums.size();
+   int k=0;
+    for(int i=0; i<n; i++)
     {
-        unordered_map<int,int> mpp;
-        int n = nums.size();
-        int k = 0;
- 
-        for(int i=0; i<n ;i++)
-        {
-            if(nums[i] != val)
-            mpp[nums[i]]++;
-
-        }
-
-        for(auto it:mpp)
-        {
-
-           while(it.second --> 0)
-            nums[k++] = it.first;
-        }
-
-        return k;
-        
+      if(nums[i] != val)
+      {
+        nums[k++] = nums[i];
+      }
+  
     }
+    return k;
+  }
 };
+
+
+
+ //     unordered_map<int,int> mpp;
+    //     int n = nums.size();
+    //     int k = 0;
+ 
+    //     for(int i=0; i<n ;i++)
+    //     {
+    //         if(nums[i] != val)
+    //         mpp[nums[i]]++;
+
+    //     }
+
+    //     for(auto it:mpp)
+    //     {
+
+    //        while(it.second --> 0)
+    //         nums[k++] = it.first;
+    //     }
+
+    //     return k;
