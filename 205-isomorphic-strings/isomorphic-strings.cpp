371100ecbb85d6064f22128c9484843bf162b4ca
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-02-16 21:22:05 +0530

    Time: 4 ms (24.45%) | Memory: 9.5 MB (27.86%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) 
+    {
+        unordered_map<char,char> mpp;
+        unordered_map<char,char> ppm;
+
+        for(int i=0; i<s.length(); i++)
+        {
+            char c1 = s[i];
+            char c2 = t[i];
+
+            if(mpp.count(c1)!=0 && mpp[c1]!=c2)
+            return false;
+
+            if(ppm.count(c2)!=0 && ppm[c2]!=c1)
+            return false;
+
+
+             mpp[c1] = c2;
+             ppm[c2] = c1;
+        }
+
+       return true;
+
+    }
+};
