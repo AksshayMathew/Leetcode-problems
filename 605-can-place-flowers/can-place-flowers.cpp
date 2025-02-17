5bbf02646b1308b1abb993d219fa5dfad0d2d5c9
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-02-17 11:11:02 +0530

    Time: 0 ms (100.00%) | Memory: 24.1 MB (41.40%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -0,0 +1,139 @@
+// class Solution {
+// public:
+//     bool canPlaceFlowers(vector<int>& flowerbed, int n) 
+//     {
+
+    
+ 
+//         int s  = flowerbed.size();
+//         int cnt = 0;
+//         for(int i=1; i<s-1; i++)
+//         {
+//             if( flowerbed[i] == 0 )
+//             {
+//                 if(flowerbed[i+1] == 0 && flowerbed[i-1] == 0)
+//                 {
+                   
+//                     flowerbed[i] = 1;
+//                      cnt++;
+//                 }
+
+//                  if(flowerbed[i+1] == 1 && flowerbed[i-1] == 0)
+//                 {
+                   
+//                     flowerbed[i-1] = 1;
+//                      cnt++;
+//                 }
+
+//                  if(flowerbed[i-1] == 1 && flowerbed[i+1] == 0 )
+//                 {
+                   
+//                     flowerbed[i+1] = 1;
+//                      cnt++;
+//                 }
+//             }
+               
+//         }
+
+//         return (cnt>=n);
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) 
+    {
+
+    
+ 
+        int s  = flowerbed.size();
+        int cnt = 0;
+
+
+
+      if(s==1)
+      {
+        if(flowerbed[0] == 0 && n<=1)
+         return true ;
+        else if(flowerbed[0] == 1 && n==0)
+        return true; 
+        else
+        return false;
+      }
+     
+
+
+
+
+       if(flowerbed[0]==0  && flowerbed[1]==0)
+       {
+        flowerbed[0] = 1;
+        cnt++;
+       }
+
+
+        for(int i=1; i<s-1; i++)
+        {
+            if( flowerbed[i] == 0 )
+            {
+                if(flowerbed[i+1] == 0 && flowerbed[i-1] == 0)
+                {
+                   
+                    flowerbed[i] = 1;
+                     cnt++;
+                }
+
+            }
+               
+        }
+
+
+        
+       if(flowerbed[s-1]==0 && flowerbed[s-2]==0)
+       {
+        flowerbed[s-1] = 1;
+        cnt++;
+       }
+
+        return (cnt>=n);
+    }
+};
