c575424190a53bab08162fca777798c192b1f77c
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-03-22 09:48:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (20.34%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -4,10 +4,11 @@ public:
     {
         string s1 = s+s;
         int n = goal.length();
+        if(s.length() != goal.length())
+        return false;
+
         for(int i=0; i<s.length(); i++) 
         {
-            if(s.length() != goal.length())
-            return false;
            string temp = s1.substr(i,n);
             if(temp == goal)
             return true;
