2cbd9e34a00e2f64dbf8fd7937a4637d127607bd
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-04-01 21:55:04 +0530

    Time: 123 ms (53.04%) | Memory: 26.2 MB (37.53%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
-    int beautySum(string s) {
+    int beautySum(string s)
+    {
+        
         int n = s.length();
         int res = 0;
 
-        for (int i = 0; i < n; i++) {
-            int freq[26] = {0}; // Fixed-size frequency array
-            for (int j = i; j < n; j++) {
-                freq[s[j] - 'a']++; // Update frequency
+        for(int i=0; i<n; i++)
+        {
+            unordered_map<char,int> mpp;
+            for(int j=i; j<n; j++)
+            {
 
-                int maxi = 0, mini = INT_MAX;
+                mpp[s[j]]++;
+        
+                int maxi = INT_MIN;
+                int mini = INT_MAX;
 
-                // Find max and min frequency (O(26) ≈ O(1))
-                for (int k = 0; k < 26; k++) {
-                    if (freq[k] > 0) { // Only consider existing chars
-                        maxi = max(maxi, freq[k]);
-                        mini = min(mini, freq[k]);
-                    }
-                }
+               for(auto it:mpp)
+               {
+                  int cnt = it.second;
+                  maxi = max(maxi,cnt);
+                  mini = min(mini,cnt);
+               }
 
-                res += (maxi - mini);
-            }
+                int beauty = maxi - mini;
+                res = res + beauty; 
         }
-
-        return res;
+          
+     }
+         return res;  
     }
 };
