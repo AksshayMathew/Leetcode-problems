dc1581a06ca70fa3b2d635116f70a2bbe4eb5ede
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-06-05 18:04:15 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (54.11%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+
+   
+    vector<int> getRow(int rowIndex) 
+    {
+     vector<int> op = {1};
+     for(int i=1; i<=rowIndex; i++)
+     {
+        int ele = op.back() * 1LL * (rowIndex-i+1)/i;
+        op.push_back(ele);
+     }
+
+     return op;
+
+    }
+};
