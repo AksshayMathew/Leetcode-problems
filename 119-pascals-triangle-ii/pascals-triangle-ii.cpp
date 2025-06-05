7cfdef28eaebcfaf2b4ecaad882be4d9990e801a
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-06-05 18:06:58 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (54.11%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -7,7 +7,7 @@ public:
      vector<int> op = {1};
      for(int i=1; i<=rowIndex; i++)
      {
-        int ele = op.back() * 1LL * (rowIndex-i+1)/i;
+        int ele = op.back() * 1LL * (rowIndex-i+1)/i ;
         op.push_back(ele);
      }
 
