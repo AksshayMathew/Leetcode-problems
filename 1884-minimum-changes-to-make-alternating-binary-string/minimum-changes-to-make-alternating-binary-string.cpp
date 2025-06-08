adec1097eda78ebce06206365762c92b98118b1f
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-06-08 21:11:16 +0530

    Time: 2 ms (18.80%) | Memory: 9.4 MB (78.20%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -7,8 +7,8 @@ public:
 
        for(int i=0 ;i<n; i++)
        {
-            char exp_zero = (i%2 == 0)?'0':'1';
-            char exp_one  = (i%2 == 0)?'1':'0';
+            char exp_zero = (i%2 == 0)?'0':'1';  //010101
+            char exp_one  = (i%2 == 0)?'1':'0';  //101010
 
             if(s[i] != exp_zero)
             cnt_start_zero++;
