86cea2156a1718b7eebc7be7edfd05013fe7d38e
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-06-08 21:32:16 +0530

    Time: 3 ms (41.22%) | Memory: 15.1 MB (43.35%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -4,15 +4,15 @@ public:
     {
         unordered_map<char,int> mpp;
         int n = words.size() ;  // return the number of words in the list
-        for(string str:words)
+        for(string &str:words)
         {
-            for(char c:str)
+            for(char &c:str)
             {
                 mpp[c]++;
             }
         }
 
-        for(auto it:mpp)
+        for(auto &it:mpp)
         {    
                 if(it.second % n != 0)
                 {
