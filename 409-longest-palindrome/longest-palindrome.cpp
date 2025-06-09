d74a758daa9b784635af82e44bcc79de706e4274
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-06-09 21:02:25 +0530

    Time: 1 ms (30.64%) | Memory: 9.3 MB (62.42%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestPalindrome(string s) 
+    {
+        unordered_map<char,int> mpp;
+        int res = 0;
+        for(char c:s)
+        {
+            mpp[c]++;
+            if(mpp[c] % 2 == 0)
+            {
+                res+=2;
+            }
+        }
+
+        for(char c1:s)
+        {
+              if(mpp[c1] % 2 != 0)
+            {
+                res+=1;
+                break;
+            }
+        }
+
+        return res;
+        
+    }
+};
