da3115f69fb24505407341a4b2c075f318dab185
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-06-26 21:57:21 +0530

    Time: 14 ms (6.96%) | Memory: 14.9 MB (9.25%) - LeetSync

--- 1044-find-common-characters/find-common-characters.cpp
+++ 1044-find-common-characters/find-common-characters.cpp
@@ -2,35 +2,51 @@
 
 class Solution {
 public:
-    vector<string> commonChars(vector<string>& words) {
-        map<char, int> freq;
+    vector<string> commonChars(vector<string>& words) 
+    {
+        map<char,int> mpp;
 
-        // Initialize with counts from first word
-        for (char c : words[0]) {
-            freq[c]++;
+        for(char c:words[0])
+        {
+           mpp[c]++;
         }
-
-        // Compare with each subsequent word
-        for (int i = 1; i < words.size(); ++i) {
-            map<char, int> temp;
-            for (char c : words[i]) {
+        
+        for(int i=1; i<words.size(); i++)
+        {
+            map<char,int> temp;
+            for(char c:words[i])
+            {
                 temp[c]++;
             }
-            for (auto it = freq.begin(); it != freq.end(); ) {
-                it->second = min(it->second, temp[it->first]);
-                if (it->second == 0) {
-                    it = freq.erase(it); // remove if not common
-                } else {
-                    ++it;
+
+            for(auto it=mpp.begin(); it!=mpp.end(); )
+            {
+                it->second = min(it->second,temp[it->first]);
+                if(it->second == 0)
+                {
+                    it = mpp.erase(it);
+                }
+                else
+                {
+                    it++;
                 }
             }
         }
 
-      
-        vector<string> result;
-        for (auto [c, count] : freq) {
-            result.insert(result.end(), count, string(1, c));
-        }
-        return result;
+        vector<string> res;
+        for(auto it:mpp)
+        {
+         for(int i=0; i<it.second; i++)
+          {
+            string ans = string (1,it.first);
+            res.push_back(ans);
+          }
+
+        } 
+
+        return res;
+
+        
+
     }
 };
