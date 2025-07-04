a94c3cbe65d54500b8c47acf3c9d0d241f65b86e
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-07-04 10:00:57 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (29.32%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -2,26 +2,52 @@ class Solution {
 public:
     int specialArray(vector<int>& nums) 
     {
-        int n =nums.size();
-        for(int i=0; i<=n; i++)  //i<n is wrong
+        int n = nums.size();
+        // vector<int> vec(n+1,0);
+        unordered_map<int,int>mpp;
+        int res = 0;
+
+        for(int i=0; i<n; i++)
         {
-            int cnt = 0;
-            for(int num : nums)
-            {
-                if(num >= i)
-            {
-               cnt++;
-            }
-
-            }
-
-            if(cnt == i)
-             return i;  
-        
+              int index = min(n,nums[i]);
+            //   vec[index]++;
+            mpp[index]++;
         }
+      
 
+
+        for(int i=n; i>=0; i--)
+        {
+            // res+=vec[i];
+            res+=mpp[i];
+            if(res == i)
+            return i;
+        }
        
         return -1;
-        
     }
 };
+
+
+
+//n^2 APPROACH
+//  int n =nums.size();
+//         for(int i=0; i<=n; i++)  //i<n is wrong
+//         {
+//             int cnt = 0;
+//             for(int num : nums)
+//             {
+//                 if(num >= i)
+//             {
+//                cnt++;
+//             }
+
+//             }
+
+//             if(cnt == i)
+//              return i;  
+        
+//         }
+
+       
+//         return -1;
