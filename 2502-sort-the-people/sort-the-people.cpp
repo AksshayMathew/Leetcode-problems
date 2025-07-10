afea5d5f120033a77962e55eb780ed5b4dfdbdc7
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-07-10 14:35:20 +0530

    Time: 13 ms (48.57%) | Memory: 31.5 MB (21.54%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -2,29 +2,21 @@ class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) 
     {
-        int n = heights.size();
         unordered_map<int,string> mpp;
-
+        int n = heights.size();
         for(int i=0; i<n; i++)
         {
             mpp[heights[i]] = names[i];
         }
-        vector<pair<int, string>> vec(mpp.begin(), mpp.end());
-
-        // for (int i = 0; i < n; i++) {
-        //     people.push_back({heights[i], names[i]});
-        // }
-
-        // Sort by height in descending order
-        sort(vec.begin(), vec.end(), [](auto &a, auto &b) {
-            return a.first > b.first;
-        });
+        sort(heights.rbegin(), heights.rend());
 
         vector<string> ans;
-        for (auto &it : vec) {
-            ans.push_back(it.second);
+        for(int i=0; i<n; i++)
+        {
+            ans.push_back(mpp[heights[i]]);
         }
 
         return ans;
     }
+    
 };
