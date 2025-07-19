70a20cef0f1b24f6ed63dadacabf4af64dbff6e9
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-07-19 16:28:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (94.53%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,28 +1,53 @@
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) 
+//     {
+//         int n = nums.size();
+//         vector<int> arr(n,1);
+//         vector<int> prefix(n,1);
+//         vector<int> suffix(n,1);
+
+//         for(int i=1; i<n; i++)
+//         {
+//             prefix[i] = prefix[i-1] * nums[i-1];
+//         }
+
+//         for(int i=n-2; i>=0; i--)    // i>0 is wrong
+//         {
+//             suffix[i] = suffix[i+1] * nums[i+1];
+//         }
+
+//         for(int i=0; i<n; i++)
+//         {
+//             arr[i] = prefix[i] * suffix[i];
+//         }
+
+//         return arr;
+        
+//     }
+// };
+
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) 
     {
         int n = nums.size();
         vector<int> arr(n,1);
-        vector<int> prefix(n,1);
-        vector<int> suffix(n,1);
+      
+       for(int i=1; i<n; i++)
+       {
+        arr[i] = arr[i-1] * nums[i-1];
+       }
 
-        for(int i=1; i<n; i++)
-        {
-            prefix[i] = prefix[i-1] * nums[i-1];
-        }
+       int suffix = 1;
+       for(int i=n-2; i>=0; i--)
+       {
+        suffix *= nums[i+1];
+        arr[i] *= suffix;
+       }
 
-        for(int i=n-2; i>=0; i--)    // i>0 is wrong
-        {
-            suffix[i] = suffix[i+1] * nums[i+1];
-        }
+       return arr;
 
-        for(int i=0; i<n; i++)
-        {
-            arr[i] = prefix[i] * suffix[i];
-        }
-
-        return arr;
-        
-    }
+    }   
+     
 };
