652c35ae27bebbbf88e03fa3e69cb8abc1d0020d
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-08-11 15:25:51 +0530

    Time: 3 ms (16.46%) | Memory: 12.5 MB (68.20%) - LeetSync

---
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) 
+    {
+        vector<int> rank(26);
+        for(int i=0; i<order.size(); i++)
+        {
+            rank[order[i]-'a'] = i;
+        }
+
+        for(int i=0; i<words.size()-1; i++)
+        {
+            string a = words[i];
+            string b = words[i+1];
+
+            int n = min(a.length(), b.length());
+
+            bool different = false;
+            for(int j=0; j<n; j++)
+            {
+                if(a[j] != b[j])
+                {
+                    if(rank[a[j]-'a'] > rank[b[j]-'a'])
+                    {
+                      return false;
+                    }
+                    different = true;
+                    break;
+
+                }
+            }
+            if(!different && a.size()>b.size())
+            {
+                return false;
+            }
+
+        }
+
+        return true; 
+    }
+};
