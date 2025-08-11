635fb7507215e96eaa10d6e0c2121cdeb34ab67d
Aksshay Mathew P <90083757+AksshayMathew@users.noreply.github.com>
2025-08-11 15:50:43 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (82.46%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) 
+    {
+        vector<int> score(n+1);
+        for(auto &it : trust)
+        {
+            score[it[0]]--;
+            score[it[1]]++;
+        }
+
+        for(int i=1; i<=n; i++)
+        {
+            if(score[i] == n-1)
+            return i;
+        }
+        
+        return -1;
+    }
+};
